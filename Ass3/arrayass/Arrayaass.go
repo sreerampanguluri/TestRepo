55e6ff9043562a62f485da1c673201c32f1c3c78
sreerampanguluri <sreerampanguluri@gmail.com>
2022-03-10 14:09:35 -0600

    jj

--- Ass3/arrayass/Arrayaass.go
+++ Ass3/arrayass/Arrayaass.go
@@ -1,19 +1,21 @@
-package Arrayass
+package main
 
 import "fmt"
 
-func main(){
-	var  givenarray = []int {2,3,4,5,6,6,7,7,8,8,2,4}
-	var x,y int
+func main() {
+	var givenarray = []int{2, 3, 4, 5, 6, 6, 7, 7, 8, 8, 2, 4}
+	var x, y int
 	var newarray []int
 
 	fmt.Println("Find Duplicate number in a array")
 
-	fmt.Printf("The given list of number as %v\n", arrayvalue)
+	fmt.Printf("The given list of number as %v\n", givenarray)
 
-	for i = 0; i < len(arrayvalue); i++ {
-		for j = i + 1; j < len(arrayvalue); j++ {
-			if arrayvalue[i] == arrayvalue[j] {
-				newarrayvalue = append(newarrayvalue, arrayvalue[i])
-	
+	for x = 0; x < len(givenarray); x++ {
+		for y = x + 1; y < len(givenarray); y++ {
+			if givenarray[x] == givenarray[y] {
+				newarray = append(newarray, givenarray[x])
+			}
+		}
+	}
 }
